8adcf1f4fc2763d4087570d238f6604d49c85c23
Pascal Hertleif <killercup@gmail.com>
2018-07-04 12:32:07 +0200

    Add some CLI params

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const [yamlFile] = process.argv.slice(2)
+const argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+      p: 'port',
+      h: 'host',
+      h: 'help',
+    },
+    default: {
+      port: 8001,
+      host: 'localhost',
+    }
+  });
 
+const yamlFile = argv._[0]
 if (!yamlFile) {
     console.error("Usage: server.js <yaml>")
     process.exit(1)
 }
 
 const thingo = new (require("./src"))(yamlFile)
-thingo.app.listen(8001, function() {
-    console.log("Listening on http://127.0.0.1:8001")
+thingo.app.listen(argv.port, argv.host, function() {
+    console.log(`Listening on http://${argv.host}:${argv.port}`)
 })
